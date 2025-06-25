df6a1916ee10ac3bd11bda8bc1b96688de9c4124
krznck <krznck@proton.me>
2025-06-25 12:13:34 +0200

    feat(tests): add simple closure tests

---
+++ tests/integration/test_extract_closures.py
@@ -0,0 +1,79 @@
+from debug import ingest_sample
+from processing.algorithms.ClosureAnalyzer import ClosureAnalyzer
+from processing.Definitions import Definitions
+from processing.GazeStream import GazeStream
+from trackers.GazePoint import GazePoint
+
+
+def test_two_closures():
+    defs = Definitions()
+    defs.blink_threshhold_ms.update(200)
+
+    # here the closures are too long to be blinks
+    pts = [
+        GazePoint(0.1, 0.1, 0),
+        GazePoint(0.1, 0.1, 0.1),
+        GazePoint(0.1, 0.1, 0.4),
+        GazePoint(None, None, 0.5),
+        GazePoint(None, None, 1),
+        GazePoint(0.5, 0.5, 1.2),
+        GazePoint(0.5, 0.5, 1.5),
+        GazePoint(0.5, 0.5, 2),
+        GazePoint(None, None, 3),
+        GazePoint(None, None, 5),
+    ]
+
+    stream = GazeStream(pts)
+    analyzer = ClosureAnalyzer(stream, defs)
+    closures = analyzer.extract_microsleeps()
+    assert len(closures) == 2
+
+    # here we reduce the time of the closures
+    pts = [
+        GazePoint(0.1, 0.1, 0),
+        GazePoint(0.1, 0.1, 0.1),
+        GazePoint(0.1, 0.1, 0.4),
+        GazePoint(None, None, 0.5),
+        GazePoint(None, None, 0.55),
+        GazePoint(None, None, 0.60),
+        GazePoint(0.5, 0.5, 1.2),
+        GazePoint(0.5, 0.5, 1.5),
+        GazePoint(0.5, 0.5, 2),
+        GazePoint(None, None, 3),
+        GazePoint(None, None, 3.15),
+    ]
+
+    stream = GazeStream(pts)
+    analyzer = ClosureAnalyzer(stream, defs)
+    closures = analyzer.extract_blinks()
+    assert len(closures) == 2
+
+
+def test_ars_technica_sample():
+    defs = Definitions()
+    defs.blink_threshhold_ms.update(400)
+
+    points = ingest_sample("ars_technica")
+    stream = GazeStream(points)
+    assert not stream.is_empty()
+
+    analyzer = ClosureAnalyzer(stream, defs)
+    blinks = analyzer.extract_blinks()
+    microsleeps = analyzer.extract_microsleeps()
+    assert len(blinks) == 6
+    assert len(microsleeps) == 3
+
+
+def test_balatro_sample():
+    defs = Definitions()
+    defs.blink_threshhold_ms.update(400)
+
+    points = ingest_sample("balatro")
+    stream = GazeStream(points)
+    assert not stream.is_empty()
+
+    analyzer = ClosureAnalyzer(stream, defs)
+    blinks = analyzer.extract_blinks()
+    microsleeps = analyzer.extract_microsleeps()
+    assert len(blinks) == 210
+    assert len(microsleeps) == 120
