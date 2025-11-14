7ddd25718997b10f82e00c5a80912037684b20db
krznck <krznck@proton.me>
2025-11-14 12:24:23 +0100

    feat: add inspecting qt compontent hierarchy
    - adds `-d` or `--developer` arguments when starting the program
    - with the argument enabled, allows pressing Ctrl+Shift+I to print out
      the component hierarchy of the Qt component under the cursor

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
+import argparse
 import sys
 
 from PyQt6.QtWidgets import QApplication
 
 from AppContext import AppContext
+from assets.resources import APP_NAME
 from visuals.MainWindow import MainWindow
 
 
 def main():
+    parser = argparse.ArgumentParser(description=f"Run {APP_NAME}.", add_help=False)
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "-d", "--developer", action="store_true", help="run in developer mode"
+    )
+    group.add_argument(
+        "-h", "--help", action="help", help="show this help message and exit"
+    )
+    args = parser.parse_args()
+
     app = QApplication([])
     context = AppContext()
-    window = MainWindow(context)
+    if args.developer:
+        window = MainWindow(context, inspection=True)
+    else:
+        window = MainWindow(context)
 
     window.show()
     sys.exit(app.exec())
--- visuals/MainWindow.py
+++ visuals/MainWindow.py
@@ -1,4 +1,7 @@
+from PyQt6 import QtCore
 from PyQt6 import QtGui
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication
 from PyQt6.QtWidgets import QHBoxLayout
 from PyQt6.QtWidgets import QListWidget
 from PyQt6.QtWidgets import QMainWindow
@@ -15,7 +18,7 @@ class MainWindow(QMainWindow):
     pages: QStackedWidget
     context: AppContext
 
-    def __init__(self, context: AppContext) -> None:
+    def __init__(self, context: AppContext, inspection: bool = False) -> None:
         super().__init__()
         self.context = context
         self.setWindowTitle(APP_NAME)
@@ -32,6 +35,50 @@ class MainWindow(QMainWindow):
         container.setLayout(main_layout)
         self.setCentralWidget(container)
 
+        self._inspection = inspection
+
     def closeEvent(self, a0: QtGui.QCloseEvent | None = None) -> None:
         self.context.disconnect_tracker()
         return super().closeEvent(a0)
+
+    def keyPressEvent(self, a0: QtGui.QKeyEvent | None) -> None:
+        if a0 is None:
+            return
+
+        # pressing Control Shift I
+        if (
+            self._inspection
+            and a0.key() == Qt.Key.Key_I
+            and a0.modifiers()
+            == (
+                QtCore.Qt.KeyboardModifier.ControlModifier
+                | QtCore.Qt.KeyboardModifier.ShiftModifier
+            )
+        ):
+            return self.inspect_widget_at_cursor()
+
+        return super().keyPressEvent(a0)
+
+    def inspect_widget_at_cursor(self):
+        pos = QtGui.QCursor.pos()
+        widget = QApplication.widgetAt(pos)
+
+        if not widget:
+            print("No wdiget found under the cursor")
+            return
+
+        print("-- Widget Inspector ---")
+        print(f"Inspecting widget at screen position {pos.x()}, {pos.y()}\n")
+
+        current = widget
+        indent = 0
+        while current:
+            class_name = current.__class__.__name__
+            obj_name = current.objectName()
+
+            prefix = " " * indent
+            print(f"{prefix} -> Class: {class_name}, Name: '{obj_name or 'Not Set'}'")
+
+            current = current.parent()
+            indent += 1
+        print("-" * 24)
